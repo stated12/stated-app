c07adea3592b47bbb3811c9d1dbfa38411f9c839
stated12 <hello@stated.in>
2026-02-17 16:47:43 +0530

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/lib/client";
+import { createClient } from "@/lib/supabase/client";
 
 export default function SignupPage() {
   const router = useRouter();
