f855441bc45b6da2da1aa8be68c4b0ec13f5fee3
stated12 <hello@stated.in>
2026-02-23 23:42:25 +0530

    Update page.tsx

--- app/u/[username]/page.tsx
+++ app/u/[username]/page.tsx
@@ -1,14 +1,16 @@
 export const dynamic = "force-dynamic";
 
-export default function TestPage({
-  params,
-}: {
-  params: { username: string };
-}) {
+export default function UserPage(
+  props: {
+    params: { username: string };
+  }
+) {
+  const username = props.params?.username;
+
   return (
     <div style={{ padding: 40 }}>
       <h1>Username param:</h1>
-      <pre>{JSON.stringify(params)}</pre>
+      <pre>{JSON.stringify(username)}</pre>
     </div>
   );
 }
