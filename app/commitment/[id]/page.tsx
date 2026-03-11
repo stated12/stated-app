f53c8e7214c2fdca18095f217f06392429a5621a
stated12 <hello@stated.in>
2026-03-11 18:53:29 +0530

    Update page.tsx

--- app/commitment/[id]/page.tsx
+++ app/commitment/[id]/page.tsx
@@ -1,17 +1,20 @@
+export const dynamic = "force-dynamic";
+
 export default function Page({
   params,
 }: {
   params: { id: string };
 }) {
 
+  const id = params?.id;
+
   return (
-    <div style={{padding:40}}>
+    <div style={{ padding: 40 }}>
       <h1>Commitment Page Debug</h1>
 
       <p>ID from URL:</p>
 
-      <pre>{params.id}</pre>
-
+      <pre>{id}</pre>
     </div>
   );
 }
