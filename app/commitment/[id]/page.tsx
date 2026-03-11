09235638e79e08248277cc916d6838dac19d1a2e
stated12 <hello@stated.in>
2026-03-11 15:22:58 +0530

    Update page.tsx

--- app/commitment/[id]/page.tsx
+++ app/commitment/[id]/page.tsx
@@ -1,38 +1,15 @@
-import { createClient } from "@/lib/supabase/server";
 import CommitmentClient from "./view";
 
-export default async function Page({
+export default function Page({
   params,
 }: {
   params: { id: string };
 }) {
 
-  const commitmentId = params.id;
-
-  const supabase = await createClient();
-
-  const { data: commitment } = await supabase
-    .from("commitments")
-    .select(`
-      *,
-      profiles:user_id (
-        username,
-        display_name,
-        avatar_url
-      ),
-      companies:company_id (
-        username,
-        name,
-        logo_url
-      )
-    `)
-    .eq("id", commitmentId)
-    .single();
-
   return (
     <CommitmentClient
-      commitment={commitment}
-      commitmentId={commitmentId}
+      commitmentId={params.id}
     />
   );
+
 }
