83f7dfbfcf3147bd9c6b9d0783c89348101ab903
stated12 <hello@stated.in>
2026-03-12 22:11:03 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: [
+      "ui-avatars.com",
+      "lxxcereaiqakpdwukaaz.supabase.co"
+    ],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "lxxcereaiqakpdwukaaz.supabase.co",
+        hostname: "ui-avatars.com"
       },
-    ],
-  },
+      {
+        protocol: "https",
+        hostname: "*.supabase.co"
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
